040c30c2ad1786da5220d6b00abb86f6e29e749c
qxchuckle <1934009145@qq.com>
2023-04-25 11:08:33 +0800

    解决跨域

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.all("/*",function(req,res,next){
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Content-Type', 'application/json;charset=utf-8');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH');
+  res.header("Access-Control-Allow-Credentials", true); // 跨域的时候是否携带cookie
   next();
 })
 // 使用接口路由，并添加api前缀
